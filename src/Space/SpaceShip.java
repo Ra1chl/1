143537b129878e645d5656d3325bcf335c88ecce
Ra1chl <raichljonas1@gmail.com>
2025-03-05 22:00:52 +0100

    Space ship

---
+++ src/Space/SpaceShip.java
@@ -0,0 +1,11 @@
+package Space;
+
+public class SpaceShip {
+    private String name;
+    private int condition = 100;
+
+    public SpaceShip(String name, int condition) {
+        this.name = name;
+        this.condition = condition;
+    }
+}
